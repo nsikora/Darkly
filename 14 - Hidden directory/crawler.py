b8a690cf0d008b7a7dbd4a111d86ecd6c13218ca
Nicolas Sikora <nsikora@student.42.fr>
2020-03-11 13:39:36 +0100

    crawler done

--- 14 - Hidden directory/crawler.py
+++ 14 - Hidden directory/crawler.py
@@ -3,23 +3,20 @@ import re
 import os
 
 def recursive(url):
-	os.system("curl " + url + "README")
-	suffix = get_info(url)
-	for i in suffix:
-		recursive(url + i + "/")
+    os.system("curl " + url + "README")
+    suffix = get_info(url)
+    for i in suffix:
+        recursive(url + i + "/")
 
 def get_info(url):
-	liste = list()
-	r = requests.get(url)
-	html_content = r.text
-	result = str(html_content).split("\n")
-	if len(result) < 10:
-		return (liste)
-	i = 4
-	while i < len(result) - 4:
-		liste.append(result[i][9:35])
-		i+=1
-	return (liste)
-
+    i = 4
+    liste = list()
+    r = requests.get(url)
+    html_content = r.text
+    result = str(html_content).split("\n")
+    while i < len(result) and len(result) > 9:
+        liste.append(result[i][9:35])
+        i +=1
+    return (liste)
 
 recursive("http://10.12.1.107/.hidden/")
